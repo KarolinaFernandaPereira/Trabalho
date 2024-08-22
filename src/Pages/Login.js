265a18e77748816909204d7e20b1d3c92ef9ed5f
GustavoBozo <gustavinhobozo@gmail.com>
2024-08-22 01:48:54 -0300

    Tela de Login e Css

---
+++ src/Pages/Login.js
@@ -0,0 +1,44 @@
+import React, { useState } from 'react';
+import "./Login.css";
+import 'primeicons/primeicons.css';
+
+function Login(){
+
+    const [senha, setSenha] = useState("");
+    const [email, setEmail] = useState("");
+
+    const handleSubmit = (event) => {
+        event.preventDefault()
+    
+    
+        if(email === "admin@gmail.com" && senha === "admin"){
+            window.location.replace("/home")
+        }
+    }
+
+
+    return(
+        <div className="container">
+            <div className='loginCard'>
+                <form onSubmit={handleSubmit} className='loginForm'>
+                    <img className='logo' src='https://static.wixstatic.com/media/91fcc4_b24b8180ab9d490a860c7c71fb8f46d7~mv2.png/v1/fill/w_340,h_100,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/Pontoon%2520-%2520Logotipo%2520CT-02_edited.png' alt='dkeodke'/>
+                    <div className='inputDiv'>
+                        <input type='email' placeholder='E-mail'
+                        onChange={(e) => setEmail(e.target.value)}
+                        className='inputLogin'/>
+                        <span className='pi pi-user'></span>
+                    </div>
+                    <div className='inputDiv'>
+                        <input type='password' placeholder='Senha'
+                        onChange={(e) => setSenha(e.target.value)}
+                        className='inputLogin'/>
+                        <span className='pi pi-lock'></span>
+                    </div>
+                    <button className='loginButton'>Entrar</button>
+                </form>
+            </div>
+        </div>
+    );
+}
+
+export default Login;
--- src/Route.js
+++ src/Route.js
@@ -5,16 +5,18 @@ import Volume from "./Pages/Volume.js";
 import Preco from "./Pages/Preco.js";
 import Agressoes from "./Pages/Agressoes.js";
 import Swap from "./Pages/Swap.js";
+import Login from "./Pages/Login.js";
 
 const AppRoutes = () => {
     return (
        <Router>
             <Routes>
-                <Route path="/" element={<Home/>}></Route>
+                <Route path="/home" element={<Home/>}></Route>
                 <Route path="/volume" element={<Volume/>}></Route>
                 <Route path="/preco" element={<Preco/>}></Route>
                 <Route path="/agressoes" element={<Agressoes/>}></Route>
                 <Route path="/swap" element={<Swap/>}></Route>
+                <Route path="/" element={<Login/>}></Route>
             </Routes>
        </Router>
     )
