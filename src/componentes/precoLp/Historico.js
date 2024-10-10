0996d5bf9d6ef4380fc3e43a7f699656d12f0f05
GustavoBozo <gustavinhobozo@gmail.com>
2024-10-10 02:55:42 -0300

    adição de gráficos com echarts

--- src/componentes/precoLp/Historico.js
+++ src/componentes/precoLp/Historico.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { Line } from 'react-chartjs-2';
 import "./Historico.css";
 import { Accordion, AccordionTab } from 'primereact/accordion';
+import EChartsReact from "echarts-for-react";
 
 import {
     Chart as ChartJS,
@@ -30,21 +31,10 @@ ChartJS.register(
 
 export const HistoricoPrecosConvencional = () => {
 
-    const options = {
-        plugins: {
-            title: {
-                display: true,
-                text: "Histórico de Preços - Fonte Convencional"
-            },
-            legend:{
-                position: "top",
-            }
-        },
-    }
-
-    const data = {
-        labels: [
-            "2023-01-02",
+    const option = {
+        xAxis: {
+          type: 'category',
+          data: ["2023-01-02",
             "2023-01-06",
             "2023-01-12",
             "2023-01-18",
@@ -85,39 +75,53 @@ export const HistoricoPrecosConvencional = () => {
             "2023-07-12",
             "2023-07-18",
             "2023-07-24",
-            "2023-07-30",
-        ],
-        datasets: [
-            {
-                label: "2023",
-                data: [70, 71, 70, 73, 70, 72, 75, 80, 60, 70, 71, 70, 73, 70, 72, 75, 80, 60, 67, 69, 68, 70, 71, 70, 73, 70, 72, 75, 80, 60, 67, 69, 68, 99, 89, 88, 87, 90, 93, 91],
-                backgroundColor: "red",
-                borderColor: "red",
-                pointStyle: false,
-            },
-            {
-                label: "2024",
-                data: [108, 100, 95, 98, 99, 89, 88, 87, 90, 93, 91, 95, 98, 99, 89, 88, 87, 90, 93, 91, 69, 68, 70, 71, 72, 73, 70, 72, 78, 81, 88, 89, 89, 88, 87, 90, 93, 91],
-                backgroundColor: "yellow",
-                borderColor: "yellow",
-                pointStyle: false,
-            },
-            {
-                label: "2025",
-                data: [125, 115, 110, 113, 116, 118, 111, 102, 103, 104, 108, 100, 100, 99, 98, 98, 93, 94, 95, 104, 100,108, 101, 102, 103, 104, 101, 101,105, 100, 103, 106, 108, 101, 102, 110, 118, 118, 117, 119],
-                backgroundColor: "purple",
-                borderColor: "purple",
-                pointStyle: false,
-            },
-            {
-                label: "2026",
-                data: [NaN, NaN, NaN, NaN, NaN, NaN, 122, 110, 112, 113, 114, 118, 120, 110, 109, 108, 103, 104, 105, 114, 110,118, 111, 112, 113, 114, 110, 119,115, 110, 113, 116, 118, 111, 112, 120, 128, 128, 127, 129],
-                backgroundColor: "blue",
-                borderColor: "blue",
-                pointStyle: false,
-            },
+            "2023-07-30",]
+        },
+        yAxis: {
+          type: 'value'
+        },
+        color: ["white"],
+        legend: {textStyle: {
+          color: "white"
+        }},
+        tooltip: {
+            trigger: 'axis'
+          },
+        series: [
+          {
+            
+            name: "2023",
+            data: [70, 71, 70, 73, 70, 72, 75, 80, 60, 70, 71, 70, 73, 70, 72, 20, 88, 25, 99, 30, 100, 45, 150, 70, 73, 70, 72, 75, 80, 60, 67, 69, 68, 99, 89, 88, 87, 90, 93, 91],
+            color: "red",
+            type: 'line',
+            showSymbol: false,
+            
+          },
+          {
+            name: "2024",
+            data: [108, 100, 50, 110, 55, 125, 40, 130, 90, 93, 91, 95, 98, 99, 89, 88, 87, 90, 93, 91, 69, 68, 70, 71, 72, 73, 70, 72, 78, 81, 88, 89, 89, 88, 87, 90, 93, 91],
+            color: "yellow",
+            type: 'line',
+            showSymbol: false,
+          },
+          {
+            name: "2025",
+            data: [125, 115, 110, 113, 116, 118, 111, 102, 103, 104, 108, 100, 100, 99, 98, 98, 93, 94, 95, 104, 100,108, 101, 102, 103, 104, 101, 101,105, 100, 103, 106, 108, 101, 102, 110, 118, 118, 117, 119],
+            color: "purple",
+            type: 'line',
+            showSymbol: false,
+          },
+          {
+            name: "2026",
+            data: [NaN, NaN, NaN, NaN, NaN, NaN, 122, 110, 112, 113, 114, 118, 120, 110, 109, 108, 103, 104, 105, 114, 110,118, 111, 112, 113, 114, 110, 119,115, 110, 113, 116, 118, 111, 112, 120, 128, 128, 127, 129],
+            color: "blue",
+            type: 'line',
+            showSymbol: false,
+          }
         ]
-    }
+      };
+
+  
 
     const [desliza, setDesliza] = useState([]);
 
@@ -131,9 +135,7 @@ export const HistoricoPrecosConvencional = () => {
                             <span> Histórico de Preços </span>
                         </>
                     }>
-                    <div className="cardGrafico3" draggable>
-                        <Line options={options} data={data}/>
-                    </div>
+                    <EChartsReact option={option}/>
                 </AccordionTab>
             </Accordion>
             
