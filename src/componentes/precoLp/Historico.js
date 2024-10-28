30fe1a57134ba1cb8cccd39bb619d0f4d68f50aa
Karolina <karolina.fernanda.pereira@gmail.com>
2024-10-28 02:51:47 -0300

    Graficos de LP e Swap

--- src/componentes/precoLp/Historico.js
+++ src/componentes/precoLp/Historico.js
@@ -1,33 +1,7 @@
 import React, { useState } from "react";
-import { Line } from 'react-chartjs-2';
 import "./Historico.css";
 import { Accordion, AccordionTab } from 'primereact/accordion';
 import EChartsReact from "echarts-for-react";
-
-import {
-    Chart as ChartJS,
-    BarElement,
-    CategoryScale,
-    LinearScale,
-    LineElement,
-    Tooltip,
-    Legend,
-    SubTitle,
-    Title,
-    plugins,
-    scales,
-} from "chart.js";
-
-ChartJS.register(
-    BarElement,
-    CategoryScale,
-    LinearScale,
-    LineElement,
-    Tooltip,
-    Legend,
-    SubTitle,
-    Title,
-);
 
 export const HistoricoPrecosConvencional = () => {
 
@@ -75,18 +49,30 @@ export const HistoricoPrecosConvencional = () => {
             "2023-07-12",
             "2023-07-18",
             "2023-07-24",
-            "2023-07-30",]
+            "2023-07-30",],
         },
         yAxis: {
-          type: 'value'
+          type: 'value',
+          splitLine: {
+            lineStyle: {
+              type: 'solid',
+              color: "#262c30"
+            },
+          },
         },
         color: ["white"],
         legend: {textStyle: {
-          color: "white"
+          color: "rgb(165, 165, 165)",
         }},
         tooltip: {
-            trigger: 'axis'
-          },
+            trigger: 'axis',
+        },
+        grid: {
+          left: '3%',
+          right: '2%',
+          bottom: '6%',
+          top: '6%'
+        },
         series: [
           {
             
@@ -94,34 +80,36 @@ export const HistoricoPrecosConvencional = () => {
             data: [70, 71, 70, 73, 70, 72, 75, 80, 60, 70, 71, 70, 73, 70, 72, 20, 88, 25, 99, 30, 100, 45, 150, 70, 73, 70, 72, 75, 80, 60, 67, 69, 68, 99, 89, 88, 87, 90, 93, 91],
             color: "red",
             type: 'line',
-            showSymbol: false,
+            symbol: 'circle',
+            itemStyle: { opacity: 0 }
             
           },
           {
             name: "2024",
             data: [108, 100, 50, 110, 55, 125, 40, 130, 90, 93, 91, 95, 98, 99, 89, 88, 87, 90, 93, 91, 69, 68, 70, 71, 72, 73, 70, 72, 78, 81, 88, 89, 89, 88, 87, 90, 93, 91],
-            color: "yellow",
+            color: "orange",
             type: 'line',
-            showSymbol: false,
+            symbol: 'circle',
+            itemStyle: { opacity: 0 }
           },
           {
             name: "2025",
             data: [125, 115, 110, 113, 116, 118, 111, 102, 103, 104, 108, 100, 100, 99, 98, 98, 93, 94, 95, 104, 100,108, 101, 102, 103, 104, 101, 101,105, 100, 103, 106, 108, 101, 102, 110, 118, 118, 117, 119],
             color: "purple",
             type: 'line',
-            showSymbol: false,
+            symbol: 'circle',
+            itemStyle: { opacity: 0 }
           },
           {
             name: "2026",
             data: [NaN, NaN, NaN, NaN, NaN, NaN, 122, 110, 112, 113, 114, 118, 120, 110, 109, 108, 103, 104, 105, 114, 110,118, 111, 112, 113, 114, 110, 119,115, 110, 113, 116, 118, 111, 112, 120, 128, 128, 127, 129],
             color: "blue",
             type: 'line',
-            showSymbol: false,
-          }
+            symbol: 'circle',
+            itemStyle: { opacity: 0 }
+          },
         ]
       };
-
-  
 
     const [desliza, setDesliza] = useState([]);
 
