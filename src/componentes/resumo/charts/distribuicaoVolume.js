fa54a24a0eec13092221d589b81851094f5c1aab
Karolina <karolina.fernanda.pereira@gmail.com>
2024-10-31 02:57:04 -0300

    Um grafico bonito e um grafico incompleto

--- src/componentes/resumo/charts/distribuicaoVolume.js
+++ src/componentes/resumo/charts/distribuicaoVolume.js
@@ -6,12 +6,12 @@ export const GraficoDistribuicaoVolume = () => {
     const option = {
         tooltip: {
           trigger: 'axis',
-          axisPointer: {
-            type: 'shadow'
-          }
         },
         legend: {
-          data: ['Profit', 'Expenses', 'Income']
+          data: ['Volume'],
+          textStyle: {
+            color: "rgb(165, 165, 165)",
+        }
         },
         grid: {
           left: '3%',
@@ -21,55 +21,51 @@ export const GraficoDistribuicaoVolume = () => {
         },
         xAxis: [
           {
-            type: 'value'
+            type: 'value',
+            splitLine: {
+                lineStyle: {
+                  type: 'solid',
+                  color: "#262c30"
+                }
+            },
           }
         ],
         yAxis: [
           {
             type: 'category',
             axisTick: {
-              show: false
+                show: false,
             },
-            data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
-          }
+            data: [0, 100, 200, 300, 400, 500, 600, 700, 800, 900, 1000, 1100, 1200, 1300, 1400, 1500, 1600, 1700, 1800, 1900, 2000],
+            splitLine: {
+                lineStyle: {
+                  type: 'solid',
+                  color: "#262c30"
+                }
+            },
+        }
         ],
         series: [
           {
-            name: 'Profit',
-            type: 'bar',
-            label: {
-              show: true,
-              position: 'inside'
-            },
-            emphasis: {
-              focus: 'series'
-            },
-            data: [200, 170, 240, 244, 200, 220, 210]
-          },
-          {
-            name: 'Income',
+            name: 'Volume',
+            color: "rgb(98, 18, 202)",
             type: 'bar',
             stack: 'Total',
             label: {
-              show: true
+              show: false
             },
-            emphasis: {
-              focus: 'series'
-            },
-            data: [320, 302, 341, 374, 390, 450, 420]
+            data: [100, 200, 200, NaN, NaN, 70, 10, 3, 3, 3, 3, NaN, NaN, NaN, 3, NaN, NaN, NaN, NaN, NaN, 3]
           },
           {
-            name: 'Expenses',
+            name: 'Volume',
+            color: "rgb(98, 18, 202)",
             type: 'bar',
             stack: 'Total',
             label: {
-              show: true,
+              show: false,
               position: 'left'
             },
-            emphasis: {
-              focus: 'series'
-            },
-            data: [-120, -132, -101, -134, -190, -230, -210]
+            data: [-80, -200, -200, NaN, NaN, -70, -10, -3, -3, -3, -3, NaN, NaN, NaN, -3, NaN, NaN, NaN, NaN, NaN, -3],
           }
         ]
     };
@@ -78,6 +74,7 @@ export const GraficoDistribuicaoVolume = () => {
 
         <>
             <EChartsReact option={option} />
+            <span style={{fontSize: "small"}}> Volume Total: 342.60 MWm </span>
         </>
     )
 }
--- src/componentes/resumo/charts/volumeDIario.js
+++ src/componentes/resumo/charts/volumeDIario.js
@@ -1,16 +1,74 @@
 import React from "react";
-
-
-
+import EChartsReact from "echarts-for-react"; //Gráficos Apache
 
 export const GraficoMapaVolumeDiario = () => {
 
+    const option = {
 
+        dataset: {
+          source: [
+            ['score', 'amount', 'product'],
+            [10, 20145, 'SE CON MEN DEZ/22'],
+            [100, 12755, 'SE CON SEM JAN/24 DEZ/24'],
+            [100, 41032, 'SE CON TRI ABR/24 JUN/24'],
+            [10, 78254, 'SE CON ANU JAN/26 DEZ/26'],
+            [100, 58212, 'SE CON ANU JAN/25 DEZ/25'],
+          ]
+        },
+        grid: { containLabel: true },
+        tooltip: {
+            trigger: 'axis',
+            axisPointer: {
+              type: 'shadow'
+            },
+        },
+        xAxis:
+        {
+            name: 'amount',
+        },
+        yAxis: {
+            type: 'category',
+            data: [
+                'SE CON MEN DEZ/22',
+                'SE CON SEM JAN/24 DEZ/24',
+                'SE CON TRI ABR/24 JUN/24',
+                'SE CON ANU JAN/26 DEZ/26',
+                'SE CON ANU JAN/25 DEZ/25',
+            ],
+        },
+        visualMap: {
+          left: 'right',
+          top: '10%',
+          min: 10,
+          max: 100,
+          text: ['Alto', 'Baixo'],
+          textStyle: {
+            color: "rgb(165, 165, 165)",
+            },
+          // Map the score column to color
+          dimension: 0,
+          inRange: {
+            color: ['#70004f', '#00a767', '#fbff00']
+          }
+        },
+        series: [
+          {
+            type: 'bar',
+            encode: {
+              // Map the "amount" column to X axis.
+              x: 'amount',
+              // Map the "product" column to Y axis
+            y: 'product'
+            }
+          },
+        ],
+      };
 
     return (
 
         <>
-            <span>Mapa Volume Diário</span>
+            <span style={{fontSize: "small"}}> Total: 14.0 MWm | Compra: 0.0 Mwm (0.0%) | Venda: 14.0 MWm (100.0%) </span>
+            <EChartsReact option={option} />
         </>
     )
 }
